17fb6a326b85e8fe8197b87914284d8a5e54416b
Acon <aconcavy@gmail.com>
2020-07-01 17:24:13 +0900

    Add ABC072 (ABCD)

---
+++ ABC072/Tests/TestD.cs
@@ -0,0 +1,54 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using D;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestD
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = @"5
+1 4 3 5 2";
+            var output = @"2";
+            Tester.InOutTest(() => Program.Solve(), input, output);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var input = @"2
+1 2";
+            var output = @"1";
+            Tester.InOutTest(() => Program.Solve(), input, output);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var input = @"2
+2 1";
+            var output = @"0";
+            Tester.InOutTest(() => Program.Solve(), input, output);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var input = @"9
+1 2 4 9 5 8 7 3 6";
+            var output = @"3";
+            Tester.InOutTest(() => Program.Solve(), input, output);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var input = @"6
+2 1 3 4 6 5";
+            var output = @"1";
+            Tester.InOutTest(() => Program.Solve(), input, output);
+        }
+    }
+}
