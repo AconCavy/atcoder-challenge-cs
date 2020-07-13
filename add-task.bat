af4dab2b501fbb2a396f1eef0f413eb057770a16
Acon <aconcavy@gmail.com>
2020-07-14 01:46:51 +0900

    Add add-task.bat which adds templates of solver and test to solution

---
+++ add-task.bat
@@ -0,0 +1,12 @@
+@echo off
+if "%~1"=="" (
+    echo Need task name
+    exit /b
+)
+set TASK=%~1
+
+dotnet new atcsolver -n %TASK%
+dotnet sln add ./%TASK%/%TASK%.csproj
+dotnet new atctests -n Test%TASK% -o ./Tests
+dotnet add ./Tests/Tests.csproj reference ./%TASK%/%TASK%.csproj
+code ./%TASK%/Program.cs
