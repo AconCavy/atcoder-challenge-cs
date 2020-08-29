f68abc4896042a1709ca6d358ae566b06097ff28
Acon <aconcavy@gmail.com>
2020-08-29 19:36:19 +0900

    Update templates and snippets

--- atcoder.bat
+++ atcoder.bat
@@ -4,6 +4,16 @@ if "%~1"=="" (
     exit /b
 )
 set SLN=%~1
-start https://atcoder.jp/contests/%SLN%
+
 dotnet new atcoder -n %SLN%
+dotnet new atcsolver -n A -o ./%SLN%/Tasks
+dotnet new atctests -n A -o ./%SLN%/Tests
+dotnet new atcsolver -n B -o ./%SLN%/Tasks
+dotnet new atctests -n B -o ./%SLN%/Tests
+dotnet new atcsolver -n C -o ./%SLN%/Tasks
+dotnet new atctests -n C -o ./%SLN%/Tests
+dotnet new atcsolver -n D -o ./%SLN%/Tasks
+dotnet new atctests -n D -o ./%SLN%/Tests
+
+start https://atcoder.jp/contests/%SLN%
 code -n . ./%SLN%/Tests/ATests.cs ./%SLN%/Tasks/A.cs ./%SLN%/Tests/BTests.cs ./%SLN%/Tasks/B.cs ./%SLN%/Tests/CTests.cs ./%SLN%/Tasks/C.cs ./%SLN%/Tests/DTests.cs ./%SLN%/Tasks/D.cs
