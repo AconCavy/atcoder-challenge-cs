3526a5755e71ae52b400df538b846418d3db5f41
AconCavy <aconcavy@gmail.com>
2020-11-01 00:26:19 +0900

    Update ARC107 (D)

--- ARC/ARC107/Tests/DTests.cs
+++ ARC/ARC107/Tests/DTests.cs
@@ -8,24 +8,16 @@ namespace Tests
         [TestMethod]
         public void TestMethod1()
         {
-            const string input = @"";
-            const string output = @"";
+            const string input = @"4 2";
+            const string output = @"2";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            const string input = @"";
-            const string output = @"";
-            Tester.InOutTest(() => Tasks.D.Solve(), input, output);
-        }
-
-        [TestMethod]
-        public void TestMethod3()
-        {
-            const string input = @"";
-            const string output = @"";
+            const string input = @"2525 425";
+            const string output = @"687232272";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
     }
