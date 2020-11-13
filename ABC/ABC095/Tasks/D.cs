cfd4ad3ae8e8a4957fb73295bb11ac497810193d
AconCavy <aconcavy@gmail.com>
2020-11-13 21:01:55 +0900

    Update ABC095 (D)

--- ABC/ABC095/Tasks/D.cs
+++ ABC/ABC095/Tasks/D.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using SandboxCSharp.Extensions;
 
 namespace Tasks
 {
@@ -18,6 +19,39 @@ namespace Tasks
 
         public static void Solve()
         {
+            var (N, C) = Scanner.Scan<int, long>();
+            var L = new (long, long)[N];
+            var R = new (long, long)[N];
+            for (var i = 0; i < N; i++)
+            {
+                var (x, v) = Scanner.Scan<long, long>();
+                L[i] = (x, v);
+                R[N - 1 - i] = (C - x, v);
+            }
+            var cumL = L.Cumulate((0L, 0L), (l, r) => (r.Item1, l.Item2 + r.Item2)).ToArray();
+            var cumR = R.Cumulate((0L, 0L), (l, r) => (r.Item1, l.Item2 + r.Item2)).ToArray();
+
+            var lMax1 = new long[N + 1];
+            var lMax2 = new long[N + 1];
+            var rMax1 = new long[N + 1];
+            var rMax2 = new long[N + 1];
+            for (var i = 1; i <= N; i++)
+            {
+                var (lx, lv) = cumL[i];
+                var (rx, rv) = cumR[i];
+                lMax1[i] = Math.Max(lMax1[i - 1], lv - lx);
+                lMax2[i] = Math.Max(lMax2[i - 1], lv - lx * 2);
+                rMax1[i] = Math.Max(rMax1[i - 1], rv - rx);
+                rMax2[i] = Math.Max(rMax2[i - 1], rv - rx * 2);
+            }
+            var answer = 0L;
+            for (var i = 0; i <= N; i++)
+            {
+                answer = Math.Max(answer, lMax1[i] + rMax2[N - i]);
+                answer = Math.Max(answer, rMax1[i] + lMax2[N - i]);
+            }
+
+            Console.WriteLine(answer);
         }
 
         public static class Scanner
@@ -34,6 +68,52 @@ namespace Tasks
             public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>());
             public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>());
             public static IEnumerable<T> ScanEnumerable<T>() => Console.ReadLine().Trim().Split(" ").Select(x => (T)Convert.ChangeType(x, typeof(T)));
+
+        }
+    }
+}
+
+namespace SandboxCSharp.Extensions
+{
+    public static partial class EnumerableExtension
+    {
+        public static IEnumerable<TAccumulate> Cumulate<TSource, TAccumulate>(this IEnumerable<TSource> source,
+            TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (func == null) throw new ArgumentNullException(nameof(func));
+            IEnumerable<TAccumulate> Inner()
+            {
+                yield return seed;
+                foreach (var item in source) yield return seed = func(seed, item);
+            }
+            return Inner();
+        }
+        public static IEnumerable<TAccumulate> Cumulate<TSource, TAccumulate>(this IEnumerable<TSource> source,
+            Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (func == null) throw new ArgumentNullException(nameof(func));
+            IEnumerable<TAccumulate> Inner()
+            {
+                TAccumulate seed = default;
+                yield return seed;
+                foreach (var item in source) yield return seed = func(seed, item);
+            }
+            return Inner();
+        }
+        public static IEnumerable<TSource> Cumulate<TSource>(this IEnumerable<TSource> source,
+            Func<TSource, TSource, TSource> func)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (func == null) throw new ArgumentNullException(nameof(func));
+            IEnumerable<TSource> Inner()
+            {
+                TSource seed = default;
+                yield return seed;
+                foreach (var item in source) yield return seed = func(seed, item);
+            }
+            return Inner();
         }
     }
 }
--- ABC/ABC095/Tests/DTests.cs
+++ ABC/ABC095/Tests/DTests.cs
@@ -8,24 +8,54 @@ namespace Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"3 20
+2 80
+9 120
+16 1";
+            var output = @"191";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"3 20
+2 80
+9 1
+16 120";
+            var output = @"192";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"1 100000000000000
+50000000000000 1";
+            var output = @"0";
+            Tester.InOutTest(() => Tasks.D.Solve(), input, output);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            const string input = @"15 10000000000
+400000000 1000000000
+800000000 1000000000
+1900000000 1000000000
+2400000000 1000000000
+2900000000 1000000000
+3300000000 1000000000
+3700000000 1000000000
+3800000000 1000000000
+4000000000 1000000000
+4100000000 1000000000
+5200000000 1000000000
+6600000000 1000000000
+8000000000 1000000000
+9300000000 1000000000
+9700000000 1000000000";
+            const string output = @"6500000000";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
     }
