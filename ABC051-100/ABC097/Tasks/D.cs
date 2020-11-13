8efd329e104c27cb38d3108d27a333980182c544
AconCavy <aconcavy@gmail.com>
2020-11-13 19:27:52 +0900

    Update ABC097 (D)

--- ABC051-100/ABC097/Tasks/D.cs
+++ ABC051-100/ABC097/Tasks/D.cs
@@ -18,6 +18,69 @@ namespace Tasks
 
         public static void Solve()
         {
+            var (N, M) = Scanner.Scan<int, int>();
+            var P = Scanner.ScanEnumerable<int>().Select(x => x - 1).ToArray();
+            var dsu = new DisjointSetUnion(N);
+            for (var i = 0; i < M; i++)
+            {
+                var (x, y) = Scanner.Scan<int, int>();
+                dsu.Merge(x - 1, y - 1);
+            }
+
+            var answer = 0;
+            for (var i = 0; i < N; i++)
+            {
+                if (dsu.IsSame(P[i], i)) answer++;
+            }
+
+            Console.WriteLine(answer);
+        }
+
+        public class DisjointSetUnion
+        {
+            private readonly int _length;
+            private readonly int[] _parentOrSize;
+            public DisjointSetUnion(int length = 0)
+            {
+                if (length < 0) throw new ArgumentOutOfRangeException(nameof(length));
+                _length = length;
+                _parentOrSize = new int[_length];
+                Array.Fill(_parentOrSize, -1);
+            }
+            public int Merge(int u, int v)
+            {
+                if (u < 0 || _length <= u) throw new ArgumentOutOfRangeException(nameof(u));
+                if (v < 0 || _length <= v) throw new ArgumentOutOfRangeException(nameof(v));
+                var (x, y) = (LeaderOf(u), LeaderOf(v));
+                if (x == y) return x;
+                if (-_parentOrSize[x] < -_parentOrSize[y]) (x, y) = (y, x);
+                _parentOrSize[x] += _parentOrSize[y];
+                _parentOrSize[y] = x;
+                return x;
+            }
+            public bool IsSame(int u, int v)
+            {
+                if (u < 0 || _length <= u) throw new ArgumentOutOfRangeException(nameof(u));
+                if (v < 0 || _length <= v) throw new ArgumentOutOfRangeException(nameof(v));
+                return LeaderOf(u) == LeaderOf(v);
+            }
+            public int LeaderOf(int v)
+            {
+                if (v < 0 || _length <= v) throw new ArgumentOutOfRangeException(nameof(v));
+                if (_parentOrSize[v] < 0) return v;
+                return _parentOrSize[v] = LeaderOf(_parentOrSize[v]);
+            }
+            public int SizeOf(int v)
+            {
+                if (v < 0 || _length <= v) throw new ArgumentOutOfRangeException(nameof(v));
+                return -_parentOrSize[LeaderOf(v)];
+            }
+            public IEnumerable<IEnumerable<int>> GetGroups()
+            {
+                var ret = new List<int>[_length].Select(x => new List<int>()).ToArray();
+                for (var i = 0; i < _length; i++) ret[LeaderOf(i)].Add(i);
+                return ret.Where(x => x.Any());
+            }
         }
 
         public static class Scanner
--- ABC051-100/ABC097/Tests/DTests.cs
+++ ABC051-100/ABC097/Tests/DTests.cs
@@ -8,24 +8,49 @@ namespace Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"5 2
+5 3 1 4 2
+1 3
+5 4";
+            var output = @"2";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"3 2
+3 2 1
+1 2
+2 3";
+            var output = @"3";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"10 8
+5 3 6 8 7 10 9 1 2 4
+3 1
+4 1
+5 9
+2 5
+6 5
+3 5
+8 9
+7 9";
+            var output = @"8";
+            Tester.InOutTest(() => Tasks.D.Solve(), input, output);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            const string input = @"5 1
+1 2 3 4 5
+1 5";
+            const string output = @"5";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
     }
