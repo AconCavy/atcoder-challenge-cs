244c8ea9ad660f355947de8cb0d3e4b53f167eaa
AconCavy <aconcavy@gmail.com>
2020-11-17 21:41:28 +0900

    Update ABC082 (D)

--- ABC/ABC082/Tasks/D.cs
+++ ABC/ABC082/Tasks/D.cs
@@ -18,6 +18,54 @@ namespace Tasks
 
         public static void Solve()
         {
+            var S = Scanner.Scan<string>();
+            var (X, Y) = Scanner.Scan<int, int>();
+            var d = new List<int>() { 0 };
+            var idx = 0;
+            foreach (var c in S)
+            {
+                if (c == 'F') d[idx]++;
+                else
+                {
+                    idx++;
+                    d.Add(0);
+                }
+            }
+
+            var dx = new List<int>();
+            var dy = new List<int>();
+            for (var i = 1; i < d.Count; i++)
+            {
+                (i % 2 == 0 ? dx : dy).Add(d[i]);
+            }
+
+            const int max = 16001;
+            const int origin = max / 2;
+            var dpx = new bool[dx.Count + 1, max];
+            var dpy = new bool[dy.Count + 1, max];
+            dpx[0, origin + d[0]] = true;
+            for (var i = 0; i < dx.Count; i++)
+            {
+                for (var j = 0; j < max; j++)
+                {
+                    if (!dpx[i, j]) continue;
+                    dpx[i + 1, j + dx[i]] = true;
+                    dpx[i + 1, j - dx[i]] = true;
+                }
+            }
+
+            dpy[0, origin] = true;
+            for (var i = 0; i < dy.Count; i++)
+            {
+                for (var j = 0; j < max; j++)
+                {
+                    if (!dpy[i, j]) continue;
+                    dpy[i + 1, j + dy[i]] = true;
+                    dpy[i + 1, j - dy[i]] = true;
+                }
+            }
+            var answer = dpx[dx.Count, origin + X] && dpy[dy.Count, origin + Y];
+            Console.WriteLine(answer ? "Yes" : "No");
         }
 
         public static class Scanner
--- ABC/ABC082/Tests/DTests.cs
+++ ABC/ABC082/Tests/DTests.cs
@@ -8,25 +8,55 @@ namespace Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"FTFFTFFF
+4 2";
+            var output = @"Yes";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"FTFFTFFF
+-2 -2";
+            var output = @"Yes";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"FF
+1 0";
+            var output = @"No";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            const string input = @"TF
+1 0";
+            const string output = @"No";
+            Tester.InOutTest(Tasks.D.Solve, input, output);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            const string input = @"FFTTFF
+0 0";
+            const string output = @"Yes";
+            Tester.InOutTest(Tasks.D.Solve, input, output);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            const string input = @"TTTT
+1 0";
+            const string output = @"No";
+            Tester.InOutTest(Tasks.D.Solve, input, output);
         }
     }
 }
