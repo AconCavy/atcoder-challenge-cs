0d8b9c65525a07419ed9b7efe038499b873a9b60
AconCavy <aconcavy@gmail.com>
2020-11-25 20:45:14 +0900

    Update ABC056 (D)

--- ABC/ABC056/Tasks/B.cs
+++ ABC/ABC056/Tasks/B.cs
@@ -18,9 +18,25 @@ namespace Tasks
 
         public static void Solve()
         {
-            var WAB = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-            var (W, A, B) = (WAB[0], WAB[1], WAB[2]);
+            var (W, A, B) = Scanner.Scan<int, int, int>();
             Console.WriteLine(Math.Max(0, Math.Max(B - (A + W), A - (B + W))));
+        }
+
+        public static class Scanner
+        {
+            private static Queue<string> queue = new Queue<string>();
+            public static T Next<T>()
+            {
+                if (!queue.Any()) foreach (var item in Console.ReadLine().Trim().Split(" ")) queue.Enqueue(item);
+                return (T)Convert.ChangeType(queue.Dequeue(), typeof(T));
+            }
+            public static T Scan<T>() => Next<T>();
+            public static (T1, T2) Scan<T1, T2>() => (Next<T1>(), Next<T2>());
+            public static (T1, T2, T3) Scan<T1, T2, T3>() => (Next<T1>(), Next<T2>(), Next<T3>());
+            public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>());
+            public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>());
+            public static (T1, T2, T3, T4, T5, T6) Scan<T1, T2, T3, T4, T5, T6>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>(), Next<T6>());
+            public static IEnumerable<T> ScanEnumerable<T>() => Console.ReadLine().Trim().Split(" ").Select(x => (T)Convert.ChangeType(x, typeof(T)));
         }
     }
 }
--- ABC/ABC056/Tasks/D.cs
+++ ABC/ABC056/Tasks/D.cs
@@ -18,14 +18,46 @@ namespace Tasks
 
         public static void Solve()
         {
-            var NK = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-            var (N, K) = (NK[0], NK[1]);
-            var A = Console.ReadLine().Trim().Split(' ').Select(long.Parse).ToArray();
+            var (N, K) = Scanner.Scan<int, int>();
+            var A = Scanner.ScanEnumerable<long>().ToArray();
+            Array.Sort(A);
+            var (l, r) = (-1, N);
+            while (r - l > 1)
+            {
+                var m = (l + r) / 2;
+                if (A[m] >= K) { r = m; continue; }
+                var dp = new bool[K];
+                dp[0] = true;
+                for (var i = 0; i < N; i++)
+                {
+                    if (i == m) continue;
+                    for (var j = K - 1 - A[i]; j >= 0; j--) dp[j + A[i]] |= dp[j];
+                }
 
-            // Console.WriteLine(string.Join("\n", dp.Select(x => string.Join(" ", x))));
+                var ok = false;
+                for (var i = K - A[m]; i < K && !ok; i++) ok |= dp[i];
+                if (ok) r = m;
+                else l = m;
+            }
 
-            Console.WriteLine(answer);
+            Console.WriteLine(r);
+        }
 
+        public static class Scanner
+        {
+            private static Queue<string> queue = new Queue<string>();
+            public static T Next<T>()
+            {
+                if (!queue.Any()) foreach (var item in Console.ReadLine().Trim().Split(" ")) queue.Enqueue(item);
+                return (T)Convert.ChangeType(queue.Dequeue(), typeof(T));
+            }
+            public static T Scan<T>() => Next<T>();
+            public static (T1, T2) Scan<T1, T2>() => (Next<T1>(), Next<T2>());
+            public static (T1, T2, T3) Scan<T1, T2, T3>() => (Next<T1>(), Next<T2>(), Next<T3>());
+            public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>());
+            public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>());
+            public static (T1, T2, T3, T4, T5, T6) Scan<T1, T2, T3, T4, T5, T6>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>(), Next<T6>());
+            public static IEnumerable<T> ScanEnumerable<T>() => Console.ReadLine().Trim().Split(" ").Select(x => (T)Convert.ChangeType(x, typeof(T)));
         }
     }
 }
