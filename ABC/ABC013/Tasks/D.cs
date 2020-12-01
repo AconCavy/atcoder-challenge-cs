5140361d4453dfb2230886b4b8cf2d041b617241
AconCavy <aconcavy@gmail.com>
2020-12-01 20:40:16 +0900

    Update ABC013 (D)

--- ABC/ABC013/Tasks/D.cs
+++ ABC/ABC013/Tasks/D.cs
@@ -18,6 +18,40 @@ namespace Tasks
 
         public static void Solve()
         {
+            var (N, M, D) = Scanner.Scan<int, int, int>();
+            var A = M > 0 ? Scanner.ScanEnumerable<int>().ToArray() : Array.Empty<int>();
+            const int upper = 32;
+            var T = new int[upper][].Select(_ => new int[N]).ToArray();
+            var answer = new int[N];
+            for (var i = 0; i < N; i++)
+            {
+                T[0][i] = i;
+                answer[i] = i;
+            }
+
+            for (var i = M - 1; i >= 0; i--)
+            {
+                (T[0][A[i]], T[0][A[i] - 1]) = (T[0][A[i] - 1], T[0][A[i]]);
+            }
+
+            for (var i = 1; i < upper; i++)
+            {
+                for (var j = 0; j < N; j++)
+                {
+                    T[i][j] = T[i - 1][T[i - 1][j]];
+                }
+            }
+
+            for (var i = 0; i < N; i++)
+            {
+                for (var j = 0; j < upper; j++)
+                {
+                    if ((D >> j & 1) == 0) continue;
+                    answer[i] = T[j][answer[i]];
+                }
+            }
+
+            Console.WriteLine(string.Join("\n", answer.Select(x => x + 1)));
         }
 
         public static class Scanner
@@ -33,6 +67,8 @@ namespace Tasks
             public static (T1, T2, T3) Scan<T1, T2, T3>() => (Next<T1>(), Next<T2>(), Next<T3>());
             public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>());
             public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>());
+            public static (T1, T2, T3, T4, T5, T6) Scan<T1, T2, T3, T4, T5, T6>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>(), Next<T6>());
+            public static IEnumerable<T> ScanEnumerable<T>() => Console.ReadLine().Trim().Split(" ").Select(x => (T)Convert.ChangeType(x, typeof(T)));
         }
     }
 }
--- ABC/ABC013/Tests/DTests.cs
+++ ABC/ABC013/Tests/DTests.cs
@@ -8,24 +8,44 @@ namespace Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"5 7 1
+1 4 3 4 2 3 1";
+            var output = @"4
+2
+5
+3
+1";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"5 7 2
+1 4 3 4 2 3 1";
+            var output = @"3
+2
+1
+5
+4";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            var input = @"";
-            var output = @"";
+            var input = @"10 20 300
+9 1 2 5 8 1 9 3 5 6 4 5 4 6 8 3 2 7 9 6";
+            var output = @"3
+7
+2
+4
+5
+9
+6
+1
+8
+10";
             Tester.InOutTest(() => Tasks.D.Solve(), input, output);
         }
     }
