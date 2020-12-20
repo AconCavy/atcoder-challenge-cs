4194941dc7f7e9cfd4a0aac2655076d776864400
AconCavy <aconcavy@gmail.com>
2020-12-20 20:31:49 +0900

    Add ARC019 (AB)

---
+++ ARC/ARC019/Tests/ATests.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ATests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"1Z0";
+            const string output = @"120";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"4ZD6O";
+            const string output = @"42060";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"BI9Z";
+            const string output = @"8192";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+    }
+}
---
+++ ARC/ARC019/Tests/BTests.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"ARC";
+            const string output = @"73";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"S";
+            const string output = @"0";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"NOLEMONNOMELON";
+            const string output = @"350";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
