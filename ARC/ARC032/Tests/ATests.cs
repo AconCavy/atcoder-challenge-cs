4bfb1051fb74b9142402016c0d198b705d6527ee
AconCavy <aconcavy@gmail.com>
2020-12-31 07:23:55 +0900

    Add ARC032 (AB)

---
+++ ARC/ARC032/Tests/ATests.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ATests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"2
+";
+            const string output = @"WANWAN
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"5
+";
+            const string output = @"BOWWOW
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"1
+";
+            const string output = @"BOWWOW
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test4()
+        {
+            const string input = @"999
+";
+            const string output = @"BOWWOW
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+    }
+}
