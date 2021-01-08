30605f03719e1c074a4713cea2356f2cd5f43e87
AconCavy <aconcavy@gmail.com>
2021-01-08 21:54:45 +0900

    Add ARC044 (AB)

---
+++ ARC/ARC044/Tests/ATests.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ATests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"42
+";
+            const string output = @"Not Prime
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"49
+";
+            const string output = @"Prime
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"3
+";
+            const string output = @"Prime
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test4()
+        {
+            const string input = @"1
+";
+            const string output = @"Not Prime
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+    }
+}
---
+++ ARC/ARC044/Tests/BTests.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"4
+0 1 1 2
+";
+            const string output = @"6
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"4
+0 1 2 0
+";
+            const string output = @"0
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"3
+1 1 2
+";
+            const string output = @"0
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test4()
+        {
+            const string input = @"17
+0 1 1 2 2 4 3 2 4 5 3 3 2 1 5 4 2
+";
+            const string output = @"855391686
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
