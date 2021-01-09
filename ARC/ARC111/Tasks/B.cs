8df6c3402a2ca69517fabfc26cc11f5e0db4f474
AconCavy <aconcavy@gmail.com>
2021-01-10 00:39:06 +0900

    Update ARC111 (B)

--- ARC/ARC111/Tasks/B.cs
+++ ARC/ARC111/Tasks/B.cs
@@ -20,6 +20,77 @@ namespace Tasks
         public static void Solve()
         {
             var N = Scanner.Scan<int>();
+            var M = 400000;
+            var dsu = new DisjointSetUnion(M);
+            var edge = new int[M];
+            for (var i = 0; i < N; i++)
+            {
+                var (a, b) = Scanner.Scan<int, int>();
+                var u = dsu.LeaderOf(a - 1);
+                var v = dsu.LeaderOf(b - 1);
+                var w = u;
+                if (!dsu.IsSame(u, v))
+                {
+                    w = dsu.Merge(u, v);
+                    edge[w] += edge[w == u ? v : u];
+                }
+                edge[w]++;
+            }
+
+            var answer = 0;
+            for (var i = 0; i < M; i++)
+            {
+                if (dsu.LeaderOf(i) == i) answer += Math.Min(edge[i], dsu.SizeOf(i));
+            }
+
+            Console.WriteLine(answer);
+        }
+
+        public class DisjointSetUnion
+        {
+            private readonly int _length;
+            private readonly int[] _parentOrSize;
+            public DisjointSetUnion(int length = 0)
+            {
+                if (length < 0) throw new ArgumentOutOfRangeException(nameof(length));
+                _length = length;
+                _parentOrSize = new int[_length];
+                Array.Fill(_parentOrSize, -1);
+            }
+            public int Merge(int u, int v)
+            {
+                if (u < 0 || _length <= u) throw new ArgumentOutOfRangeException(nameof(u));
+                if (v < 0 || _length <= v) throw new ArgumentOutOfRangeException(nameof(v));
+                var (x, y) = (LeaderOf(u), LeaderOf(v));
+                if (x == y) return x;
+                if (-_parentOrSize[x] < -_parentOrSize[y]) (x, y) = (y, x);
+                _parentOrSize[x] += _parentOrSize[y];
+                _parentOrSize[y] = x;
+                return x;
+            }
+            public bool IsSame(int u, int v)
+            {
+                if (u < 0 || _length <= u) throw new ArgumentOutOfRangeException(nameof(u));
+                if (v < 0 || _length <= v) throw new ArgumentOutOfRangeException(nameof(v));
+                return LeaderOf(u) == LeaderOf(v);
+            }
+            public int LeaderOf(int v)
+            {
+                if (v < 0 || _length <= v) throw new ArgumentOutOfRangeException(nameof(v));
+                if (_parentOrSize[v] < 0) return v;
+                return _parentOrSize[v] = LeaderOf(_parentOrSize[v]);
+            }
+            public int SizeOf(int v)
+            {
+                if (v < 0 || _length <= v) throw new ArgumentOutOfRangeException(nameof(v));
+                return -_parentOrSize[LeaderOf(v)];
+            }
+            public IEnumerable<IEnumerable<int>> GetGroups()
+            {
+                var ret = new List<int>[_length].Select(x => new List<int>()).ToArray();
+                for (var i = 0; i < _length; i++) ret[LeaderOf(i)].Add(i);
+                return ret.Where(x => x.Any());
+            }
         }
 
         public static class Printer
