ecc15ea81f9cb1c9e68a798a1eb52191e62c5e73
AconCavy <aconcavy@gmail.com>
2021-01-10 20:05:52 +0900

    Add ARC046 (AB)

---
+++ ARC/ARC046/Tests/ATests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ATests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"1
+";
+            const string output = @"1
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"11
+";
+            const string output = @"22
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"50
+";
+            const string output = @"555555
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+    }
+}
---
+++ ARC/ARC046/Tests/BTests.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"5
+3 3
+";
+            const string output = @"Takahashi
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"4
+3 3
+";
+            const string output = @"Aoki
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"5
+3 2
+";
+            const string output = @"Takahashi
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test4()
+        {
+            const string input = @"1000000000
+1000000000 1
+";
+            const string output = @"Takahashi
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
