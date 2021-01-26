23ad16283a29be3d5b645871fa517c0dad9efcdf
AconCavy <aconcavy@gmail.com>
2021-01-26 20:05:13 +0900

    Add HITACHI2020 (AB)

---
+++ ARC-Like/HITACHI2020/Tests/BTests.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"2 3 1
+3 3
+3 3 3
+1 2 1
+";
+            const string output = @"5
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"1 1 2
+10
+10
+1 1 5
+1 1 10
+";
+            const string output = @"10
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"2 2 1
+3 5
+3 5
+2 2 2
+";
+            const string output = @"6
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
