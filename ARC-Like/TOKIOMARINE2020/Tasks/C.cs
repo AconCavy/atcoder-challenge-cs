15f1cbfb461f1204d48672921111895082eef0dd
AconCavy <aconcavy@gmail.com>
2021-01-26 21:07:00 +0900

    Update TOKIOMARINE2020

---
+++ ARC-Like/TOKIOMARINE2020/Tasks/C.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Tasks
+{
+    public class C
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+            Solve();
+            Console.Out.Flush();
+        }
+
+        public static void Solve()
+        {
+            var (N, K) = Scanner.Scan<int, int>();
+            var A = Scanner.ScanEnumerable<int>().ToArray();
+            var answer = new int[N + 1];
+            Array.Copy(A, answer, N);
+            while (K-- > 0)
+            {
+                var imos = new int[N + 1];
+                for (var i = 0; i < N; i++)
+                {
+                    var l = Math.Max(0, i - answer[i]);
+                    var r = Math.Min(N, i + answer[i] + 1);
+                    imos[l]++;
+                    imos[r]--;
+                }
+                for (var i = 0; i < N; i++) imos[i + 1] = Math.Min(N, imos[i + 1] + imos[i]);
+                answer = imos;
+
+                var ok = true;
+                for (var i = 0; i < N && ok; i++)
+                {
+                    if (answer[i] < N) ok = false;
+                }
+
+                if (ok) break;
+            }
+
+            Printer.Print1D(answer[..N], " ");
+        }
+
+        public static class Printer
+        {
+            public static void Print<T>(T source) => Console.WriteLine(source);
+            public static void Print1D<T>(IEnumerable<T> source, string separator = "") =>
+                Console.WriteLine(string.Join(separator, source));
+            public static void Print1D<T, U>(IEnumerable<T> source, Func<T, U> selector, string separator = "") =>
+                Console.WriteLine(string.Join(separator, source.Select(selector)));
+            public static void Print2D<T>(IEnumerable<IEnumerable<T>> source, string separator = "") =>
+                Console.WriteLine(string.Join("\n", source.Select(x => string.Join(separator, x))));
+            public static void Print2D<T, U>(IEnumerable<IEnumerable<T>> source, Func<T, U> selector, string separator = "") =>
+                Console.WriteLine(string.Join("\n", source.Select(x => string.Join(separator, x.Select(selector)))));
+            public static void Print2D<T>(T[,] source, string separator = "")
+            {
+                var (h, w) = (source.GetLength(0), source.GetLength(1));
+                for (var i = 0; i < h; i++)
+                    for (var j = 0; j < w; j++)
+                    {
+                        Console.Write(source[i, j]);
+                        Console.Write(j == w - 1 ? "\n" : separator);
+                    }
+            }
+            public static void Print2D<T, U>(T[,] source, Func<T, U> selector, string separator = "")
+            {
+                var (h, w) = (source.GetLength(0), source.GetLength(1));
+                for (var i = 0; i < h; i++)
+                    for (var j = 0; j < w; j++)
+                    {
+                        Console.Write(selector(source[i, j]));
+                        Console.Write(j == w - 1 ? "\n" : separator);
+                    }
+            }
+        }
+
+        public static class Scanner
+        {
+            private static Queue<string> queue = new Queue<string>();
+            public static T Next<T>()
+            {
+                if (!queue.Any()) foreach (var item in Console.ReadLine().Trim().Split(" ")) queue.Enqueue(item);
+                return (T)Convert.ChangeType(queue.Dequeue(), typeof(T));
+            }
+            public static T Scan<T>() => Next<T>();
+            public static (T1, T2) Scan<T1, T2>() => (Next<T1>(), Next<T2>());
+            public static (T1, T2, T3) Scan<T1, T2, T3>() => (Next<T1>(), Next<T2>(), Next<T3>());
+            public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>());
+            public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>());
+            public static (T1, T2, T3, T4, T5, T6) Scan<T1, T2, T3, T4, T5, T6>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>(), Next<T6>());
+            public static IEnumerable<T> ScanEnumerable<T>() => Console.ReadLine().Trim().Split(" ").Select(x => (T)Convert.ChangeType(x, typeof(T)));
+        }
+    }
+}
