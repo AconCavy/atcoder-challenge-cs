4f704dcd8721116259f323a6e0384f45b359f8bf
AconCavy <aconcavy@gmail.com>
2021-01-30 20:30:04 +0900

    Add DIVERTA2019-2 (AB)

---
+++ ARC-Like/DIVERTA2019-2/Tests/BTests.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"2
+1 1
+2 2
+";
+            const string output = @"1
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"3
+1 4
+4 6
+7 8
+";
+            const string output = @"1
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"4
+1 1
+1 2
+2 1
+2 2
+";
+            const string output = @"2
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
