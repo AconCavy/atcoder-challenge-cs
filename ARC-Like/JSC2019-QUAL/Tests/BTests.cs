fb4d5e5b9736c7b559158e811177ab67794dfdf5
AconCavy <aconcavy@gmail.com>
2021-01-31 21:20:12 +0900

    Add JSC2019-QUAL (AB)

---
+++ ARC-Like/JSC2019-QUAL/Tests/BTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"2 2
+2 1
+";
+            const string output = @"3
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"3 5
+1 1 1
+";
+            const string output = @"0
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"10 998244353
+10 9 8 7 5 6 3 4 2 1
+";
+            const string output = @"185297239
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
