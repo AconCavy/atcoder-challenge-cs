e3a29c151ac793a846fd377ad5d7a6de2861edbd
AconCavy <aconcavy@gmail.com>
2021-02-01 20:27:29 +0900

    Add NIKKEI2019-2-QUAL (AB)

---
+++ ARC-Like/NIKKEI2019-2-QUAL/Tests/BTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"4
+0 1 1 2
+";
+            const string output = @"2
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"4
+1 1 1 1
+";
+            const string output = @"0
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"7
+0 3 2 1 2 2 1
+";
+            const string output = @"24
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
