829300d4d9883a7ce17312b71939de260ab9eb95
AconCavy <aconcavy@gmail.com>
2021-02-03 21:16:52 +0900

    Update AGC043

---
+++ AGC/AGC043/Tests/ATests.cs
@@ -0,0 +1,65 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ATests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"3 3
+.##
+.#.
+##.
+";
+            const string output = @"1
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"2 2
+#.
+.#
+";
+            const string output = @"2
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"4 4
+..##
+#...
+###.
+###.
+";
+            const string output = @"0
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test4()
+        {
+            const string input = @"5 5
+.#.#.
+#.#.#
+.#.#.
+#.#.#
+.#.#.
+";
+            const string output = @"4
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+    }
+}
---
+++ AGC/AGC043/Tests/BTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"4
+1231
+";
+            const string output = @"1
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"10
+2311312312
+";
+            const string output = @"0
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
