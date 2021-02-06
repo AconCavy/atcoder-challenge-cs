e74f741b598d68809b72b15522a89be74ab14359
AconCavy <aconcavy@gmail.com>
2021-02-06 20:12:11 +0900

    Add AGC004 (A)

---
+++ AGC/AGC004/Tests/BTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"2 10
+1 100
+";
+            const string output = @"12
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"3 10
+100 1 100
+";
+            const string output = @"23
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"4 10
+1 2 3 4
+";
+            const string output = @"10
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
