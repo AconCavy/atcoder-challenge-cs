a9af6b07bdf14fe1068826efb0d45159a1350f90
AconCavy <aconcavy@gmail.com>
2021-02-08 20:47:00 +0900

    Add AGC006 (A)

---
+++ AGC/AGC006/Tests/ATests.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ATests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"3
+abc
+cde
+";
+            const string output = @"5
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"1
+a
+z
+";
+            const string output = @"2
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"4
+expr
+expr
+";
+            const string output = @"4
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+    }
+}
