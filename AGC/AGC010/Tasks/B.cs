86efe46d330c5cf102b363379618280a7b7293b7
AconCavy <aconcavy@gmail.com>
2021-02-10 21:06:21 +0900

    Add AGC010 (AB)

---
+++ AGC/AGC010/Tasks/B.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Tasks
+{
+    public class B
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+            Solve();
+            Console.Out.Flush();
+        }
+
+        public static void Solve()
+        {
+            var N = Scanner.Scan<int>();
+            var A = Scanner.ScanEnumerable<long>().ToArray();
+            var all = (long)N * (N + 1) / 2;
+            var sum = A.Sum();
+            if (sum % all != 0) { Console.WriteLine("NO"); return; }
+
+            var k = sum / all;
+            var d = new long[N];
+            for (var i = 0; i < N; i++)
+            {
+                d[i] = A[i] - A[(i + 1) % N] + k;
+            }
+
+            if (d.Any(x => x < 0)) { Console.WriteLine("NO"); return; }
+
+            var count = 0L;
+            for (var i = 0; i < N; i++)
+            {
+                if (d[i] % N != 0)
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+
+                count += d[i] / N;
+            }
+
+            var answer = count == k;
+            Console.WriteLine(answer ? "YES" : "NO");
+        }
+
+        public static class Scanner
+        {
+            private static Queue<string> queue = new Queue<string>();
+            public static T Next<T>()
+            {
+                if (!queue.Any()) foreach (var item in Console.ReadLine().Trim().Split(" ")) queue.Enqueue(item);
+                return (T)Convert.ChangeType(queue.Dequeue(), typeof(T));
+            }
+            public static T Scan<T>() => Next<T>();
+            public static (T1, T2) Scan<T1, T2>() => (Next<T1>(), Next<T2>());
+            public static (T1, T2, T3) Scan<T1, T2, T3>() => (Next<T1>(), Next<T2>(), Next<T3>());
+            public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>());
+            public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>());
+            public static (T1, T2, T3, T4, T5, T6) Scan<T1, T2, T3, T4, T5, T6>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>(), Next<T6>());
+            public static IEnumerable<T> ScanEnumerable<T>() => Console.ReadLine().Trim().Split(" ").Select(x => (T)Convert.ChangeType(x, typeof(T)));
+        }
+    }
+}
