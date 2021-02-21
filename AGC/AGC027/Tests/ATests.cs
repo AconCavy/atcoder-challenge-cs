fd160d7809ff8a4e31a9e4fb943656a75fdc8454
AconCavy <aconcavy@gmail.com>
2021-02-21 20:41:00 +0900

    Add AGC027 (A)

---
+++ AGC/AGC027/Tests/ATests.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ATests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"3 70
+20 30 10
+";
+            const string output = @"2
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"3 10
+20 30 10
+";
+            const string output = @"1
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"4 1111
+1 10 100 1000
+";
+            const string output = @"4
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test4()
+        {
+            const string input = @"2 10
+20 20
+";
+            const string output = @"0
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+    }
+}
