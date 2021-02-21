6ed22d304f52855d8593e33676defcf75f38d5c4
AconCavy <aconcavy@gmail.com>
2021-02-21 23:36:52 +0900

    Add ARC113 (ABC)

---
+++ ARC/ARC113/Tests/BTests.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"4 3 2
+";
+            const string output = @"4
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"1 2 3
+";
+            const string output = @"1
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"3141592 6535897 9323846
+";
+            const string output = @"2
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test4()
+        {
+            const string input = @"2 10 3";
+            const string output = @"6";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
---
+++ ARC/ARC113/Tests/CTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"accept
+";
+            const string output = @"3
+";
+            Tester.InOutTest(Tasks.C.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"atcoder
+";
+            const string output = @"0
+";
+            Tester.InOutTest(Tasks.C.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"anerroroccurred
+";
+            const string output = @"16
+";
+            Tester.InOutTest(Tasks.C.Solve, input, output);
+        }
+    }
+}
