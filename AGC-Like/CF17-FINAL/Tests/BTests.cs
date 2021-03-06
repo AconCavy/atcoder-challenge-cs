2e1cc080acfa1ab18b49b597df2a7d1c753f78ae
AconCavy <aconcavy@gmail.com>
2021-03-07 07:59:57 +0900

    Add CF17-FINAL (ABC)

---
+++ AGC-Like/CF17-FINAL/Tests/BTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"abac
+";
+            const string output = @"YES
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"aba
+";
+            const string output = @"NO
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"babacccabab
+";
+            const string output = @"YES
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
---
+++ AGC-Like/CF17-FINAL/Tests/CTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"3
+7 12 8
+";
+            const string output = @"4
+";
+            Tester.InOutTest(Tasks.C.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"2
+11 11
+";
+            const string output = @"2
+";
+            Tester.InOutTest(Tasks.C.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"1
+0
+";
+            const string output = @"0
+";
+            Tester.InOutTest(Tasks.C.Solve, input, output);
+        }
+    }
+}
