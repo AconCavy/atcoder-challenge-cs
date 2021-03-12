0b329f2fd007010b77924c0ef0c92a19a98d134e
AconCavy <aconcavy@gmail.com>
2021-03-13 02:54:48 +0900

    Update TDPC (F)

---
+++ Other/TDPC/Tests/FTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class FTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"10 2";
+            const string output = @"21";
+            Tester.InOutTest(Tasks.F.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"10 10";
+            const string output = @"255";
+            Tester.InOutTest(Tasks.F.Solve, input, output);
+        }
+    }
+}
