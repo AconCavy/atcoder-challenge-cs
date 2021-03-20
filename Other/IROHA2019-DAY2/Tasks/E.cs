7fb742751a5aee1267a29480bf4960969a9a56f2
AconCavy <aconcavy@gmail.com>
2021-03-21 05:18:28 +0900

    Update IROHA2019-DAY2

--- Other/IROHA2019-DAY2/Tasks/E.cs
+++ Other/IROHA2019-DAY2/Tasks/E.cs
@@ -22,64 +22,32 @@ namespace Tasks
         public static void Solve()
         {
             var (N, M) = Scanner.Scan<int, int>();
-            var answer = ModuloMathematics.Combination(N + M - 2, M - 1);
+
+            var fact = new mint[N + M + 1];
+            var ifact = new mint[N + M + 1];
+            fact[0] = ifact[0] = 1;
+            for (var i = 1; i <= N + M; i++)
+            {
+                fact[i] = fact[i - 1] * i;
+                ifact[i] = 1 / fact[i];
+            }
+
+            mint Combination(int n, int k)
+            {
+                if (n < k || n < 0 || k < 0) return 0;
+                return fact[n] * ifact[k] * ifact[n - k];
+            }
+
+            var answer = Combination(N + M - 2, M - 1);
             var min = Math.Min(N, M);
             for (var k = 1; k <= min; k++)
             {
-                var odd = ModuloMathematics.Combination(k, N - k);
-                var even = ModuloMathematics.Combination(M - 1, M - k);
+                var odd = Combination(k, N - k);
+                var even = Combination(M - 1, M - k);
                 answer -= odd * even;
             }
 
             Console.WriteLine(answer);
-        }
-
-        public static class ModuloMathematics
-        {
-            private const int DefaultLength = 32;
-            private static ModuloInteger[] _factorial;
-            private static ModuloInteger[] _factorialInverse;
-            private static int _length;
-            static ModuloMathematics()
-            {
-                _factorial = new ModuloInteger[DefaultLength + 1];
-                _factorial[0] = 1;
-                _factorialInverse = new ModuloInteger[DefaultLength + 1];
-                _factorialInverse[0] = 1;
-                Initialize(DefaultLength);
-            }
-            public static ModuloInteger Combination(int n, int r)
-            {
-                if (n < 0 || r < 0 || n < r) return 0;
-                if (_length < n) Initialize(n);
-                return _factorial[n] * _factorialInverse[r] * _factorialInverse[n - r];
-            }
-            public static ModuloInteger Permutation(int n, int r)
-            {
-                if (n < 0 || r < 0 || n < r) return 0;
-                if (_length < n) Initialize(n);
-                return _factorial[n] * _factorialInverse[n - r];
-            }
-            public static ModuloInteger Factorial(int n)
-            {
-                if (n < 0) throw new ArgumentException(nameof(n));
-                if (_length < n) Initialize(n);
-                return _factorial[n];
-            }
-            private static void Initialize(int n)
-            {
-                if (_length < n)
-                {
-                    Array.Resize(ref _factorial, n + 1);
-                    Array.Resize(ref _factorialInverse, n + 1);
-                }
-                for (var i = _length + 1; i <= n; i++)
-                {
-                    _factorial[i] = _factorial[i - 1] * i;
-                    _factorialInverse[i] = 1 / _factorial[i];
-                }
-                _length = n;
-            }
         }
 
         public readonly struct ModuloInteger : IEquatable<ModuloInteger>
