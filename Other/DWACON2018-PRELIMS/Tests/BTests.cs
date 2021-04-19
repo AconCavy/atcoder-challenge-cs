de965d5afa06208a084095bc0b1583e2c94c1c3d
AconCavy <aconcavy@gmail.com>
2021-04-19 21:21:14 +0900

    Add DWACON2018-PRELIMS (AB)

---
+++ Other/DWACON2018-PRELIMS/Tests/BTests.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"225525
+";
+            const string output = @"2
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"52
+";
+            const string output = @"-1
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"2255252252222555552522255255
+";
+            const string output = @"5
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test4()
+        {
+            const string input = @"25252
+";
+            const string output = @"-1
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
