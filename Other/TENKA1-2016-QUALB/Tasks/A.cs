4b9fb77293804848acb608615edfa123f1117187
AconCavy <aconcavy@gmail.com>
2021-05-05 21:41:14 +0900

    Add TENKA1-2016-QUALB (AB)

---
+++ Other/TENKA1-2016-QUALB/Tasks/A.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Tasks
+{
+    public class A
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+            Solve();
+            Console.Out.Flush();
+        }
+
+        public static void Solve()
+        {
+            long F(long x) => (x * x + 4) / 8;
+
+            var answer = F(F(F(20)));
+            Console.WriteLine(answer);
+        }
+    }
+}
