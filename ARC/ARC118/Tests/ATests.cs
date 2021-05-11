27f13d60b4ae566ddc372a2d57b8a6372fbcc6c0
AconCavy <aconcavy@gmail.com>
2021-05-11 22:03:41 +0900

    Add ARC118 (A)

---
+++ ARC/ARC118/Tests/ATests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ATests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"10 1
+";
+            const string output = @"10
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"3 5
+";
+            const string output = @"171
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"1 1000000000
+";
+            const string output = @"100999999999
+";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+    }
+}
