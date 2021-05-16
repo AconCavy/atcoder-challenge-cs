cc827f6318631a746a7935e26491ce99dfcc9b31
AconCavy <aconcavy@gmail.com>
2021-05-17 06:04:36 +0900

    Add ATC002 (AB)

---
+++ Other/ATC002/Tests/BTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"12 15 7
+";
+            const string output = @"3
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"123456789 234567894 6574837563712
+";
+            const string output = @"120678297
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
