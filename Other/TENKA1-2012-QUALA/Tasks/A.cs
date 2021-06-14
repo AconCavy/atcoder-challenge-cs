5153d0483ad681ddd036d1ff45cfe323163f09bd
AconCavy <aconcavy@gmail.com>
2021-06-15 05:10:31 +0900

    TENKA1-2012-QUALA

---
+++ Other/TENKA1-2012-QUALA/Tasks/A.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Tasks
+{
+    public class A
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+            Solve();
+            Console.Out.Flush();
+        }
+
+        public static void Solve()
+        {
+            var N = Scanner.Scan<int>();
+            var dp = new long[N + 2];
+            Array.Fill(dp, -1);
+            dp[0] = dp[1] = 1;
+
+            long Fibonacci(int x)
+            {
+                if (dp[x] != -1) return dp[x];
+                return dp[x] = Fibonacci(x - 1) + Fibonacci(x - 2);
+            }
+
+
+            var answer = Fibonacci(N);
+            Console.WriteLine(answer);
+        }
+
+        public static class Scanner
+        {
+            private static Queue<string> queue = new Queue<string>();
+            public static T Next<T>()
+            {
+                if (!queue.Any()) foreach (var item in Console.ReadLine().Trim().Split(" ")) queue.Enqueue(item);
+                return (T)Convert.ChangeType(queue.Dequeue(), typeof(T));
+            }
+            public static T Scan<T>() => Next<T>();
+            public static (T1, T2) Scan<T1, T2>() => (Next<T1>(), Next<T2>());
+            public static (T1, T2, T3) Scan<T1, T2, T3>() => (Next<T1>(), Next<T2>(), Next<T3>());
+            public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>());
+            public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>());
+            public static (T1, T2, T3, T4, T5, T6) Scan<T1, T2, T3, T4, T5, T6>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>(), Next<T6>());
+            public static IEnumerable<T> ScanEnumerable<T>() => Console.ReadLine().Trim().Split(" ").Select(x => (T)Convert.ChangeType(x, typeof(T)));
+        }
+    }
+}
---
+++ Other/TENKA1-2012-QUALA/Tests/ATests.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ATests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"0";
+            const string output = @"1";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"5";
+            const string output = @"8";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"45";
+            const string output = @"1836311903";
+            Tester.InOutTest(Tasks.A.Solve, input, output);
+        }
+    }
+}
