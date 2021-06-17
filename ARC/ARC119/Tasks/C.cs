e1dd23beafb4cb043edc9b94f1d13d08ef4fe964
AconCavy <aconcavy@gmail.com>
2021-06-17 10:01:18 +0900

    Update ARC119

--- ARC/ARC119/Tasks/C.cs
+++ ARC/ARC119/Tasks/C.cs
@@ -19,6 +19,42 @@ namespace Tasks
 
         public static void Solve()
         {
+            var N = Scanner.Scan<int>();
+            var A = Scanner.ScanEnumerable<long>().ToArray();
+            var cum = new long[N + 1];
+            for (var i = 0; i < N; i++)
+            {
+                var b = i % 2 == 0 ? A[i] : -A[i];
+                cum[i + 1] += cum[i] + b;
+            }
+
+            var dict = new Dictionary<long, long>();
+            var answer = 0L;
+            foreach (var c in cum)
+            {
+                if (!dict.ContainsKey(c)) dict[c] = 0;
+                answer += dict[c];
+                dict[c]++;
+            }
+
+            Console.WriteLine(answer);
+        }
+
+        public static class Scanner
+        {
+            private static Queue<string> queue = new Queue<string>();
+            public static T Next<T>()
+            {
+                if (!queue.Any()) foreach (var item in Console.ReadLine().Trim().Split(" ")) queue.Enqueue(item);
+                return (T)Convert.ChangeType(queue.Dequeue(), typeof(T));
+            }
+            public static T Scan<T>() => Next<T>();
+            public static (T1, T2) Scan<T1, T2>() => (Next<T1>(), Next<T2>());
+            public static (T1, T2, T3) Scan<T1, T2, T3>() => (Next<T1>(), Next<T2>(), Next<T3>());
+            public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>());
+            public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>());
+            public static (T1, T2, T3, T4, T5, T6) Scan<T1, T2, T3, T4, T5, T6>() => (Next<T1>(), Next<T2>(), Next<T3>(), Next<T4>(), Next<T5>(), Next<T6>());
+            public static IEnumerable<T> ScanEnumerable<T>() => Console.ReadLine().Trim().Split(" ").Select(x => (T)Convert.ChangeType(x, typeof(T)));
         }
     }
 }
--- ARC/ARC119/Tests/CTests.cs
+++ ARC/ARC119/Tests/CTests.cs
@@ -11,24 +11,44 @@ namespace Tests
         [TestMethod, Timeout(TimeLimit)]
         public void Test1()
         {
-            const string input = @"";
-            const string output = @"";
+            const string input = @"5
+5 8 8 6 6
+";
+            const string output = @"3
+";
             Tester.InOutTest(Tasks.C.Solve, input, output);
         }
 
         [TestMethod, Timeout(TimeLimit)]
         public void Test2()
         {
-            const string input = @"";
-            const string output = @"";
+            const string input = @"7
+12 8 11 3 3 13 2
+";
+            const string output = @"3
+";
             Tester.InOutTest(Tasks.C.Solve, input, output);
         }
 
         [TestMethod, Timeout(TimeLimit)]
         public void Test3()
         {
-            const string input = @"";
-            const string output = @"";
+            const string input = @"10
+8 6 3 9 5 4 7 2 1 10
+";
+            const string output = @"1
+";
+            Tester.InOutTest(Tasks.C.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test4()
+        {
+            const string input = @"14
+630551244 683685976 249199599 863395255 667330388 617766025 564631293 614195656 944865979 277535591 390222868 527065404 136842536 971731491
+";
+            const string output = @"8
+";
             Tester.InOutTest(Tasks.C.Solve, input, output);
         }
     }
