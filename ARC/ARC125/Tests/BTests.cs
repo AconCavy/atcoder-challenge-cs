d43d5b191516b712d0c39db508137c621b24fc4f
AconCavy <aconcavy@gmail.com>
2021-08-23 01:34:29 +0900

    Add ARC125 (AB)

---
+++ ARC/ARC125/Tests/BTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BTests
+    {
+        const int TimeLimit = 2000;
+        const double RelativeError = 1e-9;
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test1()
+        {
+            const string input = @"3
+";
+            const string output = @"2
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test2()
+        {
+            const string input = @"10
+";
+            const string output = @"8
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [TestMethod, Timeout(TimeLimit)]
+        public void Test3()
+        {
+            const string input = @"10000000000
+";
+            const string output = @"52583544
+";
+            Tester.InOutTest(Tasks.B.Solve, input, output);
+        }
+    }
+}
