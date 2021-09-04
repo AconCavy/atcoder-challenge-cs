8b7129a79f54a8fa681db5571a46dde4b7ee1aaf
AconCavy <aconcavy@gmail.com>
2021-09-05 00:09:07 +0900

    Update ABC217 (D)

--- ABC/ABC217/Tasks/D.cs
+++ ABC/ABC217/Tasks/D.cs
@@ -20,7 +20,100 @@ namespace Tasks
         public static void Solve()
         {
             var (L, Q) = Scanner.Scan<int, int>();
-            var cut = new List<long> { 0, L };
+            var points = new List<int> { 0, L };
+            var query = new (int C, int X)[Q];
+
+            for (var i = 0; i < Q; i++)
+            {
+                var (c, x) = Scanner.Scan<int, int>();
+                query[i] = (c, x);
+                points.Add(x);
+            }
+
+            points.Sort();
+            var map = new Dictionary<int, int>();
+            var remap = new Dictionary<int, int>();
+            foreach (var (x, i) in points.Distinct().Select((x, i) => (x, i)))
+            {
+                map[x] = i;
+                remap[i] = x;
+            }
+
+            var ft = new FenwickTree(map.Count);
+            ft.Add(map[L], 1);
+            foreach (var (c, x) in query)
+            {
+                var mx = map[x];
+                if (c == 1)
+                {
+                    ft.Add(mx, 1);
+                }
+                else
+                {
+                    var sx = ft.Sum(mx);
+                    var ub = ft.UpperBound(sx);
+                    var lb = ft.LowerBound(sx);
+                    var answer = remap[ub] - remap[lb];
+                    Console.WriteLine(answer);
+                }
+            }
+        }
+
+        public class FenwickTree
+        {
+            private readonly long[] _data;
+            private readonly int _length;
+            private readonly Compare _lowerBound = (item, data) => item <= data;
+            private readonly Compare _upperBound = (item, data) => item < data;
+            public FenwickTree(int length = 0)
+            {
+                if (length < 0) throw new ArgumentOutOfRangeException(nameof(length));
+                _length = length;
+                _data = new long[length];
+            }
+            public void Add(int index, long item)
+            {
+                if (index < 0 || _length <= index) throw new ArgumentOutOfRangeException(nameof(index));
+                index++;
+                while (index <= _length)
+                {
+                    _data[index - 1] += item;
+                    index += index & -index;
+                }
+            }
+            public long Sum(int length)
+            {
+                if (length < 0 || _length < length) throw new ArgumentOutOfRangeException(nameof(length));
+                var s = 0L;
+                while (length > 0)
+                {
+                    s += _data[length - 1];
+                    length -= length & -length;
+                }
+                return s;
+            }
+            public long Sum(int left, int right)
+            {
+                if (left < 0 || right < left || _length < right) throw new ArgumentOutOfRangeException();
+                return Sum(right) - Sum(left);
+            }
+            public int LowerBound(long item) => CommonBound(item, _lowerBound);
+            public int UpperBound(long item) => CommonBound(item, _upperBound);
+            private int CommonBound(long item, Compare compare)
+            {
+                if (compare(item, 0)) return 0;
+                var x = 0;
+                var r = 1;
+                while (r < _length) r <<= 1;
+                for (var k = r; k > 0; k >>= 1)
+                {
+                    if (x + k - 1 >= _length || compare(item, _data[x + k - 1])) continue;
+                    item -= _data[x + k - 1];
+                    x += k;
+                }
+                return x;
+            }
+            private delegate bool Compare(long item, long data);
         }
 
         public static class Scanner
