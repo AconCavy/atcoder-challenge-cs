051daf0f43e726b86115f0ea08015d46ca97dd10
AconCavy <aconcavy@gmail.com>
2021-11-16 06:14:08 +0900

    Update devcontainer

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 TEMPLATEVERSION="1.0.0"
 
+mkdir tmp
 wget https://github.com/AconCavy/CompetitiveProgrammingTemplateCSharp/releases/download/v$TEMPLATEVERSION/AconCavy.CompetitiveProgramming.Templates.$TEMPLATEVERSION.nupkg -O tmp/AconCavy.CompetitiveProgramming.Templates.$TEMPLATEVERSION.nupkg
 dotnet new -i tmp/AconCavy.CompetitiveProgramming.Templates.$TEMPLATEVERSION.nupkg
