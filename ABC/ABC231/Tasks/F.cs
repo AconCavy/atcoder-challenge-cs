2b313ae49c85de259b74ab8c9317a30b9cc52416
AconCavy <aconcavy@gmail.com>
2021-12-14 07:54:43 +0900

    Update ABC231

--- ABC/ABC231/Tasks/F.cs
+++ ABC/ABC231/Tasks/F.cs
@@ -22,19 +22,32 @@ namespace Tasks
             var N = Scanner.Scan<int>();
             var A = Scanner.ScanEnumerable<int>().ToArray();
             var B = Scanner.ScanEnumerable<int>().ToArray();
-            var (ca, rca) = Compress(A);
-            var (cb, rcb) = Compress(B);
-            var C = new (int A, int B)[N];
+            var (ca, _) = Compress(A);
+            var (cb, _) = Compress(B);
+            var dict = new Dictionary<(int A, int B), int>();
             for (var i = 0; i < N; i++)
             {
-                C[i] = (ca[A[i]], cb[B[i]]);
+                var ab = (ca[A[i]], cb[B[i]]);
+                if (!dict.ContainsKey(ab)) dict[ab] = 0;
+                dict[ab]++;
+            }
+            // A[i] >= A[j] && B[i] <= B[j]
+            var ABC = dict.Select(x => (A: x.Key.A, B: x.Key.B, C: x.Value)).ToArray();
+            Array.Sort(ABC, (x, y) =>
+            {
+                var result = y.A.CompareTo(x.A);
+                return result == 0 ? x.B.CompareTo(y.B) : result;
+            });
+
+            var answer = 0L;
+            var ft = new FenwickTree(N);
+            foreach (var (a, b, c) in ABC)
+            {
+                ft.Add(b, c);
+                answer += ft.Sum(b + 1) * c;
             }
 
-            Array.Sort(C, (x, y) =>
-            {
-                var result = x.A.CompareTo(y.A);
-                return result == 0 ? y.B.CompareTo(x.B) : result;
-            });
+            Console.WriteLine(answer);
         }
 
         public static (Dictionary<T, int> Map, Dictionary<int, T> ReMap) Compress<T>(IEnumerable<T> source)
@@ -49,6 +62,62 @@ namespace Tasks
                 remap[i] = x;
             }
             return (map, remap);
+        }
+
+        public class FenwickTree
+        {
+            private readonly long[] _data;
+            private readonly int _length;
+            public FenwickTree(int length = 0)
+            {
+                if (length < 0) throw new ArgumentOutOfRangeException(nameof(length));
+                _length = length;
+                _data = new long[length];
+            }
+            public void Add(int index, long item)
+            {
+                if (index < 0 || _length <= index) throw new ArgumentOutOfRangeException(nameof(index));
+                index++;
+                while (index <= _length)
+                {
+                    _data[index - 1] += item;
+                    index += index & -index;
+                }
+            }
+            public long Sum(int length)
+            {
+                if (length < 0 || _length < length) throw new ArgumentOutOfRangeException(nameof(length));
+                var s = 0L;
+                while (length > 0)
+                {
+                    s += _data[length - 1];
+                    length -= length & -length;
+                }
+                return s;
+            }
+            public long Sum(int left, int right)
+            {
+                if (left < 0 || right < left || _length < right) throw new ArgumentOutOfRangeException();
+                return Sum(right) - Sum(left);
+            }
+            public int LowerBound(long item) => CommonBound(item, LessThanOrEqual);
+            public int UpperBound(long item) => CommonBound(item, LessThan);
+            private int CommonBound(long item, Func<long, long, bool> compare)
+            {
+                if (compare(item, _data[0])) return 0;
+                var x = 0;
+                var r = 1;
+                while (r < _length) r <<= 1;
+                for (var k = r; k > 0; k >>= 1)
+                {
+                    if (x + k - 1 >= _length || compare(item, _data[x + k - 1])) continue;
+                    item -= _data[x + k - 1];
+                    x += k;
+                }
+                return x;
+            }
+            private static bool LessThanOrEqual(long x, long y) => x <= y;
+            private static bool LessThan(long x, long y) => x < y;
         }
 
         public static class Scanner
