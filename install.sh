3dd14f073060e3f6e07472c7eb012bdf8167e3ff
AconCavy <aconcavy@gmail.com>
2022-02-06 03:20:57 +0900

    Update install scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEMPLATEVERSION="1.2.1"
+TEMPLATEVERSION="1.2.0"
 
 mkdir tmp
 wget https://github.com/AconCavy/CompetitiveProgrammingTemplateCSharp/releases/download/v$TEMPLATEVERSION/AconCavy.CompetitiveProgramming.Templates.$TEMPLATEVERSION.nupkg -O tmp/AconCavy.CompetitiveProgramming.Templates.$TEMPLATEVERSION.nupkg
