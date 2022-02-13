3df13bd24c6ce62890525d370a6012d21a290a53
AconCavy <aconcavy@gmail.com>
2022-02-13 23:40:08 +0900

    Update install scripts

--- install.cmd
+++ install.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-set TEMPLATEVERSION=1.2.0
+set TEMPLATEVERSION=1.2.1
 
 powershell -Command "New-Item tmp -ItemType directory; Invoke-WebRequest -Uri https://github.com/AconCavy/CompetitiveProgrammingTemplateCSharp/releases/download/v%TEMPLATEVERSION%/AconCavy.CompetitiveProgramming.Templates.%TEMPLATEVERSION%.nupkg -OutFile ./tmp/AconCavy.CompetitiveProgramming.Templates.%TEMPLATEVERSION%.nupkg; dotnet new -i ./tmp/AconCavy.CompetitiveProgramming.Templates.%TEMPLATEVERSION%.nupkg"
