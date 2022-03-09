160dbb1ed7c63813c86a4e1138c9bf343255bd96
AconCavy <aconcavy@gmail.com>
2022-03-09 15:46:26 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ TEMPLATE_VERSION="1.2.1"
 PACKAGE="AconCavy.CompetitiveProgramming.Templates.$TEMPLATE_VERSION.nupkg"
 URI="https://github.com/AconCavy/CompetitiveProgrammingTemplateCSharp/releases/download/v$TEMPLATE_VERSION/$PACKAGE"
 OUTPUT_DIR="tmp"
-PACKAGE_PATH="./$OUTPUT_DIR%/$PACKAGE"
+PACKAGE_PATH="./$OUTPUT_DIR/$PACKAGE"
 
 mkdir $OUTPUT_DIR
 curl -L $URI -o $PACKAGE_PATH
