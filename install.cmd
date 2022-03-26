b2babab204f0416814f47ff8d462677703b3d0b7
AconCavy <aconcavy@gmail.com>
2022-03-26 20:20:48 +0900

    Update install scripts

--- install.cmd
+++ install.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set TEMPLATE_VERSION=2.0.0
+set TEMPLATE_VERSION=2.0.1
 set PACKAGE=AconCavy.CompetitiveProgramming.Templates.%TEMPLATE_VERSION%.nupkg
 set URI=https://github.com/AconCavy/cp-template-csharp/releases/download/v%TEMPLATE_VERSION%/%PACKAGE%
 set OUTPUT_DIR=tmp
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEMPLATE_VERSION="2.0.0"
+TEMPLATE_VERSION="2.0.1"
 PACKAGE="AconCavy.CompetitiveProgramming.Templates.$TEMPLATE_VERSION.nupkg"
 URI="https://github.com/AconCavy/cp-template-csharp/releases/download/v$TEMPLATE_VERSION/$PACKAGE"
 OUTPUT_DIR="tmp"
