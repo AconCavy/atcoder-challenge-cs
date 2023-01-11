83cfca3f39c06f4860280bd5b408823344269138
AconCavy <aconcavy@gmail.com>
2023-01-11 10:37:57 +0900

    Update ABC284 F

--- ABC/ABC284/F.cs
+++ ABC/ABC284/F.cs
@@ -50,30 +50,27 @@ namespace Tasks
             private const ulong Mask31 = (1UL << 31) - 1;
             private const ulong Modulo = (1UL << 61) - 1;
             private const ulong Positivizer = Modulo * ((1UL << 3) - 1);
-            private const int MaxPowerLength = (int)2e6;
-            private static readonly ulong[] Powers = new ulong[MaxPowerLength + 1];
             private static readonly ulong Base;
             static RollingHash()
             {
                 Base = (ulong)new Random().Next(1 << 8, 1 << 30);
-                Powers[0] = 1;
-                for (var i = 0; i + 1 < Powers.Length; i++)
-                {
-                    Powers[i + 1] = CalcModulo(Multiply(Powers[i], Base));
-                }
             }
+            private readonly ulong[] _powers;
             private readonly ulong[] _hash;
             public RollingHash(ReadOnlySpan<char> s)
             {
+                _powers = new ulong[s.Length + 1];
+                _powers[0] = 1;
                 _hash = new ulong[s.Length + 1];
                 for (var i = 0; i < s.Length; i++)
                 {
+                    _powers[i + 1] = CalcModulo(Multiply(_powers[i], Base));
                     _hash[i + 1] = CalcModulo(Multiply(_hash[i], Base) + s[i]);
                 }
             }
             public ulong Slice(int i, int length)
             {
-                return CalcModulo(_hash[i + length] + Positivizer - Multiply(_hash[i], Powers[length]));
+                return CalcModulo(_hash[i + length] + Positivizer - Multiply(_hash[i], _powers[length]));
             }
             private static ulong Multiply(ulong a, ulong b)
             {
