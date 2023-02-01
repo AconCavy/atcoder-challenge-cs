611b613b74a93c2a513ad2f12bbd44e85a0924f3
AconCavy <aconcavy@gmail.com>
2023-02-02 01:18:57 +0900

    Update DP

---
+++ Other/DP/F.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Tasks
+{
+    public class F
+    {
+        public static void Main()
+        {
+            using var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+            Solve();
+            Console.Out.Flush();
+        }
+
+        public static void Solve()
+        {
+            var S = Scanner.Scan<string>();
+            var T = Scanner.Scan<string>();
+            var N = S.Length;
+            var M = T.Length;
+            var dp = new int[N + 1, M + 1];
+            for (var i = 0; i < N; i++)
+            {
+                for (var j = 0; j < M; j++)
+                {
+                    if (S[i] == T[j]) dp[i + 1, j + 1] = dp[i, j] + 1;
+                    else dp[i + 1, j + 1] = Math.Max(dp[i + 1, j], dp[i, j + 1]);
+                }
+            }
+
+            var si = N;
+            var ti = M;
+            var buffer = new List<char>();
+            while (si > 0 || ti > 0)
+            {
+                var len = dp[si, ti];
+                if (si > 0 && dp[si - 1, ti] == len)
+                {
+                    si--;
+                }
+                else if (ti > 0 && dp[si, ti - 1] == len)
+                {
+                    ti--;
+                }
+                else
+                {
+                    si--; ti--;
+                    buffer.Add(S[si]);
+                }
+            }
+
+            buffer.Reverse();
+            var answer = string.Join("", buffer);
+            Console.WriteLine(answer);
+        }
+
+        public static class Scanner
+        {
+            public static T Scan<T>() where T : IConvertible => Convert<T>(Scan()[0]);
+            public static (T1, T2) Scan<T1, T2>() where T1 : IConvertible where T2 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]));
+            }
+            public static (T1, T2, T3) Scan<T1, T2, T3>() where T1 : IConvertible where T2 : IConvertible where T3 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]), Convert<T3>(buffer[2]));
+            }
+            public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() where T1 : IConvertible where T2 : IConvertible where T3 : IConvertible where T4 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]), Convert<T3>(buffer[2]), Convert<T4>(buffer[3]));
+            }
+            public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() where T1 : IConvertible where T2 : IConvertible where T3 : IConvertible where T4 : IConvertible where T5 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]), Convert<T3>(buffer[2]), Convert<T4>(buffer[3]), Convert<T5>(buffer[4]));
+            }
+            public static (T1, T2, T3, T4, T5, T6) Scan<T1, T2, T3, T4, T5, T6>() where T1 : IConvertible where T2 : IConvertible where T3 : IConvertible where T4 : IConvertible where T5 : IConvertible where T6 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]), Convert<T3>(buffer[2]), Convert<T4>(buffer[3]), Convert<T5>(buffer[4]), Convert<T6>(buffer[5]));
+            }
+            public static IEnumerable<T> ScanEnumerable<T>() where T : IConvertible => Scan().Select(Convert<T>);
+            private static string[] Scan()
+            {
+                var line = Console.ReadLine()?.Trim() ?? string.Empty;
+                return string.IsNullOrEmpty(line) ? Array.Empty<string>() : line.Split(' ');
+            }
+            private static T Convert<T>(string value) where T : IConvertible => (T)System.Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
---
+++ Other/DP/Tests.cs
@@ -0,0 +1,530 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Timeout(2000)]
+        [TestCase(
+        @"4
+10 30 40 20
+",
+        @"30
+",
+        TestName = "{m}-1")]
+        [TestCase(
+        @"2
+10 10
+",
+        @"0
+",
+        TestName = "{m}-2")]
+        [TestCase(
+        @"6
+30 10 60 10 60 50
+",
+        @"40
+",
+        TestName = "{m}-3")]
+        public void ATest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.A.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"5 3
+10 30 40 50 20
+",
+            @"30
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"3 1
+10 20 10
+",
+            @"20
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"2 100
+10 10
+",
+            @"0
+",
+            TestName = "{m}-3")]
+        [TestCase(
+            @"10 4
+40 10 20 70 80 10 20 70 80 60
+",
+            @"40
+",
+            TestName = "{m}-4")]
+        public void BTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.B.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"3
+10 40 70
+20 50 80
+30 60 90
+",
+            @"210
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"1
+100 10 1
+",
+            @"100
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"7
+6 7 8
+8 8 3
+2 5 2
+7 8 6
+4 6 8
+2 3 4
+7 5 1
+",
+            @"46
+",
+            TestName = "{m}-3")]
+        public void CTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.C.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"3 8
+3 30
+4 50
+5 60
+",
+            @"90
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"5 5
+1 1000000000
+1 1000000000
+1 1000000000
+1 1000000000
+1 1000000000
+",
+            @"5000000000
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"6 15
+6 5
+5 6
+6 4
+6 6
+3 5
+7 2
+",
+            @"17
+",
+            TestName = "{m}-3")]
+        public void DTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.D.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"3 8
+3 30
+4 50
+5 60
+",
+            @"90
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"1 1000000000
+1000000000 10
+",
+            @"10
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"6 15
+6 5
+5 6
+6 4
+6 6
+3 5
+7 2
+",
+            @"17
+",
+            TestName = "{m}-3")]
+        public void ETest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.E.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"axyb
+abyxb
+",
+            @"axb
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"aa
+xayaz
+",
+            @"aa
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"a
+z
+",
+            @"
+",
+            TestName = "{m}-3")]
+        [TestCase(
+            @"abracadabra
+avadakedavra
+",
+            @"aaadara
+",
+            TestName = "{m}-4")]
+        public void FTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.F.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"4 5
+1 2
+1 3
+3 2
+2 4
+3 4
+",
+            @"3
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"6 3
+2 3
+4 5
+5 6
+",
+            @"2
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"5 8
+5 3
+2 3
+2 4
+5 2
+5 1
+1 4
+4 3
+1 3
+",
+            @"3
+",
+            TestName = "{m}-3")]
+        public void GTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.G.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"3 4
+...#
+.#..
+....
+",
+            @"3
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"5 2
+..
+#.
+..
+.#
+..
+",
+            @"0
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"5 5
+..#..
+.....
+#...#
+.....
+..#..
+",
+            @"24
+",
+            TestName = "{m}-3")]
+        [TestCase(
+            @"20 20
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+",
+            @"345263555
+",
+            TestName = "{m}-4")]
+        public void HTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.H.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"3
+0.30 0.60 0.80
+",
+            @"0.612
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"1
+0.50
+",
+            @"0.5
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"5
+0.42 0.01 0.42 0.99 0.42
+",
+            @"0.3821815872
+",
+            TestName = "{m}-3")]
+        public void ITest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.I.Solve, input, output, 1e-9);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"3
+1 1 1
+",
+            @"5.5
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"1
+3
+",
+            @"3
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"2
+1 2
+",
+            @"4.5
+",
+            TestName = "{m}-3")]
+        [TestCase(
+            @"10
+1 3 2 3 3 2 3 2 1 3
+",
+            @"54.48064457488221
+",
+            TestName = "{m}-4")]
+        public void JTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.J.Solve, input, output, 1e-9);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"2 4
+2 3
+",
+            @"First
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"2 5
+2 3
+",
+            @"Second
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"2 7
+2 3
+",
+            @"First
+",
+            TestName = "{m}-3")]
+        [TestCase(
+            @"3 20
+1 2 3
+",
+            @"Second
+",
+            TestName = "{m}-4")]
+        [TestCase(
+            @"3 21
+1 2 3
+",
+            @"First
+",
+            TestName = "{m}-5")]
+        [TestCase(
+            @"1 100000
+1
+",
+            @"Second
+",
+            TestName = "{m}-6")]
+        public void KTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.K.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"4
+10 80 90 30
+",
+            @"10
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"3
+10 100 10
+",
+            @"-80
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"1
+10
+",
+            @"10
+",
+            TestName = "{m}-3")]
+        [TestCase(
+            @"10
+1000000000 1 1000000000 1 1000000000 1 1000000000 1 1000000000 1
+",
+            @"4999999995
+",
+            TestName = "{m}-4")]
+        [TestCase(
+            @"6
+4 2 9 7 1 5
+",
+            @"2
+",
+            TestName = "{m}-5")]
+        public void LTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.L.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"3 4
+1 2 3
+",
+            @"5
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"1 10
+9
+",
+            @"0
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"2 0
+0 0
+",
+            @"1
+",
+            TestName = "{m}-3")]
+        [TestCase(
+            @"4 100000
+100000 100000 100000 100000
+",
+            @"665683269
+",
+            TestName = "{m}-4")]
+        public void MTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.M.Solve, input, output);
+        }
+
+        [Timeout(2000)]
+        [TestCase(
+            @"4
+10 20 30 40
+",
+            @"190
+",
+            TestName = "{m}-1")]
+        [TestCase(
+            @"5
+10 10 10 10 10
+",
+            @"120
+",
+            TestName = "{m}-2")]
+        [TestCase(
+            @"3
+1000000000 1000000000 1000000000
+",
+            @"5000000000
+",
+            TestName = "{m}-3")]
+        [TestCase(
+            @"6
+7 6 8 6 1 1
+",
+            @"68
+",
+            TestName = "{m}-4")]
+        public void NTest(string input, string output)
+        {
+            Utility.InOutTest(Tasks.N.Solve, input, output);
+        }
+    }
+}
