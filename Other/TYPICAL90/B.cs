f4a0f21ee2a7344094e8bc10c2d7a961710a3ed1
AconCavy <aconcavy@gmail.com>
2023-03-04 04:00:46 +0900

    Update TYPICAL90

---
+++ Other/TYPICAL90/B.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Tasks
+{
+    public class B
+    {
+        public static void Main()
+        {
+            using var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+            Solve();
+            Console.Out.Flush();
+        }
+
+        public static void Solve()
+        {
+            var N = Scanner.Scan<int>();
+            if (N % 2 == 1) return;
+            var buffer = new char[N];
+
+            bool F(ReadOnlySpan<char> s)
+            {
+                var level = 0;
+                foreach (var c in s)
+                {
+                    if (c == '(') level++;
+                    else level--;
+                    if (level < 0) return false;
+                }
+
+                return level == 0;
+            }
+
+            IEnumerable<string> G(int n)
+            {
+                for (var s = 0; s < 1 << N; s++)
+                {
+                    for (var i = 0; i < N; i++)
+                    {
+                        buffer[i] = (s >> i & 1) == 0 ? '(' : ')';
+                    }
+
+
+                    if (F(buffer)) yield return new string(buffer);
+                }
+            }
+
+            var answers = G(N).OrderBy(x => x);
+            Console.WriteLine(string.Join(Environment.NewLine, answers));
+        }
+
+        public static class Scanner
+        {
+            public static T Scan<T>() where T : IConvertible => Convert<T>(Scan()[0]);
+            public static (T1, T2) Scan<T1, T2>() where T1 : IConvertible where T2 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]));
+            }
+            public static (T1, T2, T3) Scan<T1, T2, T3>() where T1 : IConvertible where T2 : IConvertible where T3 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]), Convert<T3>(buffer[2]));
+            }
+            public static (T1, T2, T3, T4) Scan<T1, T2, T3, T4>() where T1 : IConvertible where T2 : IConvertible where T3 : IConvertible where T4 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]), Convert<T3>(buffer[2]), Convert<T4>(buffer[3]));
+            }
+            public static (T1, T2, T3, T4, T5) Scan<T1, T2, T3, T4, T5>() where T1 : IConvertible where T2 : IConvertible where T3 : IConvertible where T4 : IConvertible where T5 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]), Convert<T3>(buffer[2]), Convert<T4>(buffer[3]), Convert<T5>(buffer[4]));
+            }
+            public static (T1, T2, T3, T4, T5, T6) Scan<T1, T2, T3, T4, T5, T6>() where T1 : IConvertible where T2 : IConvertible where T3 : IConvertible where T4 : IConvertible where T5 : IConvertible where T6 : IConvertible
+            {
+                var buffer = Scan();
+                return (Convert<T1>(buffer[0]), Convert<T2>(buffer[1]), Convert<T3>(buffer[2]), Convert<T4>(buffer[3]), Convert<T5>(buffer[4]), Convert<T6>(buffer[5]));
+            }
+            public static IEnumerable<T> ScanEnumerable<T>() where T : IConvertible => Scan().Select(Convert<T>);
+            private static string[] Scan()
+            {
+                var line = Console.ReadLine()?.Trim() ?? string.Empty;
+                return string.IsNullOrEmpty(line) ? Array.Empty<string>() : line.Split(' ');
+            }
+            private static T Convert<T>(string value) where T : IConvertible => (T)System.Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
