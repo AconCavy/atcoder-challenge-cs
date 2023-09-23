9b617b772949d3b63e4ccbb08f168c5a5e361500
AconCavy <aconcavy@gmail.com>
2023-09-24 03:49:31 +0900

    Update ABC321

--- ABC/ABC321/D.cs
+++ ABC/ABC321/D.cs
@@ -42,93 +42,18 @@ public class D
         Console.WriteLine(answer);
     }
 
-    public static int LowerBound<T>(ReadOnlyMemory<T> source, T key) where T : IComparable<T>
+    public static int LowerBound<T>(List<T> source, T key) where T : IComparable<T>
+        => LowerBound(System.Runtime.InteropServices.CollectionsMarshal.AsSpan(source), key);
+    public static int LowerBound<T>(ReadOnlySpan<T> source, T key) where T : IComparable<T>
     {
-        bool F(int i) => source.Span[i].CompareTo(key) >= 0;
-        return BinarySearch(-1, source.Length, F);
-    }
-    public static int LowerBound<T, U>(ReadOnlyMemory<T> source, Func<T, U> map, U key) where U : IComparable<U>
-    {
-        bool F(int i) => map(source.Span[i]).CompareTo(key) >= 0;
-        return BinarySearch(-1, source.Length, F);
-    }
-    public static int UpperBound<T>(ReadOnlyMemory<T> source, T key) where T : IComparable<T>
-    {
-        bool F(int i) => source.Span[i].CompareTo(key) > 0;
-        return BinarySearch(-1, source.Length, F);
-    }
-    public static int UpperBound<T, U>(ReadOnlyMemory<T> source, Func<T, U> map, U key) where U : IComparable<U>
-    {
-        bool F(int i) => map(source.Span[i]).CompareTo(key) > 0;
-        return BinarySearch(-1, source.Length, F);
-    }
-    public static T BinarySearch<T>(T ng, T ok, Func<T, bool> f) where T : INumber<T> => BinarySearch(ng, ok, f, T.One);
-    public static T BinarySearch<T>(T ng, T ok, Func<T, bool> f, T eps) where T : INumber<T>
-    {
-        var one = T.One;
-        var two = one + one;
-        while (T.Abs(ok - ng) > eps)
+        var (l, r) = (-1, source.Length);
+        while (r - l > 1)
         {
-            var m = ng + (ok - ng) / two;
-            if (f(m)) ok = m;
-            else ng = m;
+            var m = l + (r - l) / 2;
+            if (source[m].CompareTo(key) >= 0) r = m;
+            else l = m;
         }
-        return ok;
-    }
-
-    public class FenwickTree<T>
-        where T : struct, IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>, IComparisonOperators<T, T, bool>
-    {
-        public int Length { get; }
-        private readonly T[] _data;
-        public FenwickTree(int length)
-        {
-            if (length < 0) throw new ArgumentOutOfRangeException(nameof(length));
-            Length = length;
-            _data = new T[length];
-        }
-        public void Add(int index, T value)
-        {
-            if (index < 0 || Length <= index) throw new ArgumentOutOfRangeException(nameof(index));
-            index++;
-            while (index <= Length)
-            {
-                _data[index - 1] += value;
-                index += index & -index;
-            }
-        }
-        public T Sum(int length)
-        {
-            if (length < 0 || Length < length) throw new ArgumentOutOfRangeException(nameof(length));
-            T s = default;
-            while (length > 0)
-            {
-                s += _data[length - 1];
-                length -= length & -length;
-            }
-            return s;
-        }
-        public T Sum(int left, int right)
-        {
-            if (left < 0 || right < left || Length < right) throw new ArgumentOutOfRangeException();
-            return Sum(right) - Sum(left);
-        }
-        public int LowerBound(T value) => Bound(value, (x, y) => x <= y);
-        public int UpperBound(T value) => Bound(value, (x, y) => x < y);
-        private int Bound(T value, Func<T, T, bool> compare)
-        {
-            if (Length == 0 || compare(value, _data[0])) return 0;
-            var x = 0;
-            var r = 1;
-            while (r < Length) r <<= 1;
-            for (var k = r; k > 0; k >>= 1)
-            {
-                if (x + k - 1 >= Length || compare(value, _data[x + k - 1])) continue;
-                value -= _data[x + k - 1];
-                x += k;
-            }
-            return x;
-        }
+        return r;
     }
 
     public static class Scanner
