be2676a49121849ec7d262160c12c4699f3bffde
AconCavy <aconcavy@gmail.com>
2023-11-26 00:33:30 +0900

    Update the scripts

--- install.cmd
+++ install.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set TEMPLATE_VERSION=3.0.2
+set TEMPLATE_VERSION=3.0.3
 set PACKAGE=AconCavy.CompetitiveProgramming.Templates.%TEMPLATE_VERSION%.nupkg
 set URI=https://github.com/AconCavy/cp-template-csharp/releases/download/v%TEMPLATE_VERSION%/%PACKAGE%
 set OUTPUT_DIR=tmp
@@ -8,4 +8,4 @@ set PACKAGE_PATH=./%OUTPUT_DIR%/%PACKAGE%
 
 mkdir %OUTPUT_DIR%
 powershell -Command "Invoke-WebRequest -Uri %URI% -OutFile %PACKAGE_PATH%"
-dotnet new -i %PACKAGE_PATH%
+dotnet new install %PACKAGE_PATH%
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEMPLATE_VERSION="3.0.2"
+TEMPLATE_VERSION="3.0.3"
 PACKAGE="AconCavy.CompetitiveProgramming.Templates.$TEMPLATE_VERSION.nupkg"
 URI="https://github.com/AconCavy/cp-template-csharp/releases/download/v$TEMPLATE_VERSION/$PACKAGE"
 OUTPUT_DIR="tmp"
@@ -8,4 +8,4 @@ PACKAGE_PATH="./$OUTPUT_DIR/$PACKAGE"
 
 mkdir $OUTPUT_DIR
 curl -L $URI -o $PACKAGE_PATH
-dotnet new -i $PACKAGE_PATH
+dotnet new install $PACKAGE_PATH
