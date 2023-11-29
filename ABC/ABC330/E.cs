0e7fcc8aecddb3f5e5b627508e2c2f8f342aa1a2
AconCavy <aconcavy@gmail.com>
2023-11-29 09:18:35 +0900

    Update ABC330

--- ABC/ABC330/E.cs
+++ ABC/ABC330/E.cs
@@ -21,171 +21,62 @@ public class E
     {
         var (N, Q) = Scanner.Scan<int, int>();
         var A = Scanner.ScanEnumerable<int>().ToArray();
-        var M = N + 10;
-        var count = new int[M];
-        var st = new SegmentTree<int>(M, new Oracle());
-        const int Inf = 1 << 30;
-        for (var i = 0; i <= N; i++)
+        var count = new int[N + 1];
+        var mex = new Mex(N);
+        foreach (var a in A.Where(x => x < N))
         {
-            st.Set(i, i);
-        }
-
-        foreach (var a in A)
-        {
-            if (a < M)
-            {
-                count[a]++;
-                st.Set(a, Inf);
-            }
+            count[a]++;
+            mex.Add(a);
         }
 
         for (var q = 0; q < Q; q++)
         {
             var (i, x) = Scanner.Scan<int, int>();
             i--;
-            if (A[i] < M)
+
+            if (A[i] < N)
             {
                 count[A[i]]--;
-                if (count[A[i]] == 0) st.Set(A[i], A[i]);
+                if (count[A[i]] == 0) mex.Remove(A[i]);
             }
 
             A[i] = x;
 
-            if (x < M)
+            if (x < N)
             {
                 count[x]++;
-                st.Set(x, Inf);
+                mex.Add(x);
             }
 
-            var mex = Math.Min(st.QueryToAll(), N);
-            Console.WriteLine(mex);
+            var answer = Math.Min(mex.Get(), N);
+            Console.WriteLine(answer);
         }
     }
 
-    public class Oracle : IOracle<int>
+    public class Mex
     {
-        public int IdentityElement => 1 << 30;
+        public int RightBound { get; init; }
+        private readonly SortedSet<int> _heap;
+        public Mex(int rightBound)
+        {
+            RightBound = rightBound;
+            _heap = new SortedSet<int>(Enumerable.Range(0, rightBound + 1));
+        }
 
-        public int Operate(int a, int b)
+        public void Add(int x)
         {
-            return Math.Min(a, b);
+            _heap.Remove(x);
         }
-    }
 
-    public interface IOracle<TMonoid>
-    {
-        TMonoid IdentityElement { get; }
-        TMonoid Operate(TMonoid a, TMonoid b);
-    }
+        public int Get()
+        {
+            return _heap.Count > 0 ? _heap.Min : RightBound;
+        }
 
-    public class SegmentTree<TMonoid>
-    {
-        public int Length { get; }
-        private readonly IOracle<TMonoid> _oracle;
-        private readonly TMonoid[] _data;
-        private readonly int _log;
-        private readonly int _dataSize;
-        public SegmentTree(IReadOnlyCollection<TMonoid> source, IOracle<TMonoid> oracle) : this(source.Count, oracle)
+        public void Remove(int x)
         {
-            var idx = _dataSize;
-            foreach (var value in source) _data[idx++] = value;
-            for (var i = _dataSize - 1; i >= 1; i--) Update(i);
+            _heap.Add(Math.Min(x, RightBound));
         }
-        public SegmentTree(int length, IOracle<TMonoid> oracle)
-        {
-            if (length < 0) throw new ArgumentOutOfRangeException(nameof(length));
-            Length = length;
-            _oracle = oracle;
-            while (1 << _log < Length) _log++;
-            _dataSize = 1 << _log;
-            _data = new TMonoid[_dataSize << 1];
-            Array.Fill(_data, oracle.IdentityElement);
-        }
-        public void Set(int index, TMonoid value)
-        {
-            if (index < 0 || Length <= index) throw new ArgumentOutOfRangeException(nameof(index));
-            index += _dataSize;
-            _data[index] = value;
-            for (var i = 1; i <= _log; i++) Update(index >> i);
-        }
-        public TMonoid Get(int index)
-        {
-            if (index < 0 || Length <= index) throw new ArgumentOutOfRangeException(nameof(index));
-            return _data[index + _dataSize];
-        }
-        public TMonoid Query(int left, int right)
-        {
-            if (left < 0 || right < left || Length < right) throw new ArgumentOutOfRangeException();
-            var (sml, smr) = (_oracle.IdentityElement, _oracle.IdentityElement);
-            left += _dataSize;
-            right += _dataSize;
-            while (left < right)
-            {
-                if ((left & 1) == 1) sml = _oracle.Operate(sml, _data[left++]);
-                if ((right & 1) == 1) smr = _oracle.Operate(_data[--right], smr);
-                left >>= 1;
-                right >>= 1;
-            }
-            return _oracle.Operate(sml, smr);
-        }
-        public TMonoid QueryToAll() => _data[1];
-        public int MaxRight(int left, Func<TMonoid, bool> predicate)
-        {
-            if (left < 0 || Length < left) throw new ArgumentOutOfRangeException(nameof(left));
-            if (predicate is null) throw new ArgumentNullException(nameof(predicate));
-            if (!predicate(_oracle.IdentityElement)) throw new ArgumentException(nameof(predicate));
-            if (left == Length) return Length;
-            left += _dataSize;
-            var sm = _oracle.IdentityElement;
-            do
-            {
-                while ((left & 1) == 0) left >>= 1;
-                if (!predicate(_oracle.Operate(sm, _data[left])))
-                {
-                    while (left < _dataSize)
-                    {
-                        left <<= 1;
-                        var tmp = _oracle.Operate(sm, _data[left]);
-                        if (!predicate(tmp)) continue;
-                        sm = tmp;
-                        left++;
-                    }
-                    return left - _dataSize;
-                }
-                sm = _oracle.Operate(sm, _data[left]);
-                left++;
-            } while ((left & -left) != left);
-            return Length;
-        }
-        public int MinLeft(int right, Func<TMonoid, bool> predicate)
-        {
-            if (right < 0 || Length < right) throw new ArgumentOutOfRangeException(nameof(right));
-            if (predicate is null) throw new ArgumentNullException(nameof(predicate));
-            if (!predicate(_oracle.IdentityElement)) throw new ArgumentException(nameof(predicate));
-            if (right == 0) return 0;
-            right += _dataSize;
-            var sm = _oracle.IdentityElement;
-            do
-            {
-                right--;
-                while (right > 1 && (right & 1) == 1) right >>= 1;
-                if (!predicate(_oracle.Operate(_data[right], sm)))
-                {
-                    while (right < _dataSize)
-                    {
-                        right = (right << 1) + 1;
-                        var tmp = _oracle.Operate(_data[right], sm);
-                        if (!predicate(tmp)) continue;
-                        sm = tmp;
-                        right--;
-                    }
-                    return right + 1 - _dataSize;
-                }
-                sm = _oracle.Operate(_data[right], sm);
-            } while ((right & -right) != right);
-            return 0;
-        }
-        private void Update(int k) => _data[k] = _oracle.Operate(_data[k << 1], _data[(k << 1) + 1]);
     }
 
     public static class Scanner
