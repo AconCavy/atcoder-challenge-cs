92a150051f4e1477aeaeb94c33f0c616756741f7
AconCavy <aconcavy@gmail.com>
2023-12-16 23:53:04 +0900

    Update ABC333

--- ABC/ABC333/E.cs
+++ ABC/ABC333/E.cs
@@ -28,32 +28,34 @@ public class E
         }
 
         var answers = new List<int>();
-        var ft = new FenwickTree<int>(N);
+        var requires = new int[N];
+        var sum = 0;
         var k = 0;
         for (var i = N - 1; i >= 0; i--)
         {
             var (t, x) = queries[i];
             if (t == 1)
             {
-                var c = ft.Sum(x, x + 1);
-                if (c == 0)
+                if (requires[x] == 0)
                 {
                     answers.Add(0);
                 }
                 else
                 {
                     answers.Add(1);
-                    k = Math.Max(k, ft.Sum(N));
-                    ft.Add(x, -1);
+                    k = Math.Max(k, sum);
+                    requires[x]--;
+                    sum--;
                 }
             }
             else
             {
-                ft.Add(x, 1);
+                requires[x]++;
+                sum++;
             }
         }
 
-        if (ft.Sum(N) > 0)
+        if (sum > 0)
         {
             Console.WriteLine(-1);
         }
@@ -65,62 +67,7 @@ public class E
         }
     }
 
-    public class FenwickTree<T>
-        where T : struct, IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>, IComparisonOperators<T, T, bool>
-    {
-        public int Length { get; }
-        private readonly T[] _data;
-        public FenwickTree(int length)
-        {
-            if (length < 0) throw new ArgumentOutOfRangeException(nameof(length));
-            Length = length;
-            _data = new T[length];
-        }
-        public void Add(int index, T value)
-        {
-            if (index < 0 || Length <= index) throw new ArgumentOutOfRangeException(nameof(index));
-            index++;
-            while (index <= Length)
-            {
-                _data[index - 1] += value;
-                index += index & -index;
-            }
-        }
-        public T Sum(int length)
-        {
-            if (length < 0 || Length < length) throw new ArgumentOutOfRangeException(nameof(length));
-            T s = default;
-            while (length > 0)
-            {
-                s += _data[length - 1];
-                length -= length & -length;
-            }
-            return s;
-        }
-        public T Sum(int left, int right)
-        {
-            if (left < 0 || right < left || Length < right) throw new ArgumentOutOfRangeException();
-            return Sum(right) - Sum(left);
-        }
-        public int LowerBound(T value) => Bound(value, (x, y) => x <= y);
-        public int UpperBound(T value) => Bound(value, (x, y) => x < y);
-        private int Bound(T value, Func<T, T, bool> compare)
-        {
-            if (Length == 0 || compare(value, _data[0])) return 0;
-            var x = 0;
-            var r = 1;
-            while (r < Length) r <<= 1;
-            for (var k = r; k > 0; k >>= 1)
-            {
-                if (x + k - 1 >= Length || compare(value, _data[x + k - 1])) continue;
-                value -= _data[x + k - 1];
-                x += k;
-            }
-            return x;
-        }
-    }
-
-    public readonly record struct Query(int t, int x);
+    public readonly record struct Query(int T, int X);
 
     public static class Scanner
     {
